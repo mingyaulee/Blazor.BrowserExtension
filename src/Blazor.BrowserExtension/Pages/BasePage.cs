3e2b7dc08906c09645eb44031ad2135c0976944e
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-06-05 17:15:42 +0800

    Deprecate properties and methods in BasePage to reduce dependency injection overhead

--- src/Blazor.BrowserExtension/Pages/BasePage.cs
+++ src/Blazor.BrowserExtension/Pages/BasePage.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using System;
 using System.Threading.Tasks;
 using WebExtension.Net;
 
@@ -12,26 +13,32 @@ namespace Blazor.BrowserExtension.Pages
         [Inject]
         public IWebExtensionApi WebExtension { get; set; }
 
+        [Obsolete("This property is deprecated and will be removed in the next minor version release. Add @inject NavigationManager Navigation to continue using it.")]
         [Inject]
         public NavigationManager Navigation { get; set; }
 
+        [Obsolete("This property is deprecated and will be removed in the next minor version release. Add @inject IWebAssemblyHostEnvironment HostEnvironment to continue using it.")]
         [Inject]
         public IWebAssemblyHostEnvironment HostEnvironment { get; set; }
 
         [Inject]
         public ILogger<BasePage> Logger { get; set; }
 
+        [Obsolete("This property is deprecated and will be removed in the next minor version release. Add @inject IJSRuntime JavaScript to continue using it.")]
         [Inject]
         public IJSRuntime JavaScript { get; set; }
 
+        [Obsolete("This property is deprecated and will be removed in the next minor version release. Add @inject BrowserExtensionOption BrowserExtensionOption to continue using it.")]
         [Inject]
         public BrowserExtensionOption BrowserExtensionOption { get; set; }
 
+        [Obsolete("This method is deprecated and will be removed in the next minor version release. Please open an issue in GitHub if you think it needs to be retained.")]
         protected string GetBrowserExtension()
         {
             return $"globalThis.BlazorBrowserExtension.{BrowserExtensionOption.GetSafeProjectNamespace()}";
         }
 
+        [Obsolete("This method is deprecated and will be removed in the next minor version release. Use ES6 import instead.", UrlFormat = "https://docs.microsoft.com/en-us/aspnet/core/blazor/javascript-interoperability/call-javascript-from-dotnet?view=aspnetcore-5.0#javascript-isolation-in-javascript-modules")]
         protected async Task ImportAsync(string script)
         {
             await JavaScript.InvokeVoidAsync($"{GetBrowserExtension()}.ImportAsync", script);
--- src/Blazor.BrowserExtension/Pages/IndexPage.cs
+++ src/Blazor.BrowserExtension/Pages/IndexPage.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Linq;
 
 namespace Blazor.BrowserExtension.Pages
 {
     public class IndexPage : BasePage
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         protected override void OnParametersSet()
         {
-            var uri = Navigation.ToAbsoluteUri(Navigation.Uri);
+            var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
             var query = QueryHelpers.ParseQuery(uri.Query);
             if (query.TryGetValue("path", out var paths))
             {
                 var path = paths.FirstOrDefault();
                 if (!string.IsNullOrEmpty(path))
                 {
-                    Navigation.NavigateTo(path);
+                    NavigationManager.NavigateTo(path);
                 }
             }
             base.OnParametersSet();
