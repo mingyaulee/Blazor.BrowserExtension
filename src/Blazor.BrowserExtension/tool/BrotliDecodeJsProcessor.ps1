4ff40c89a734eb2d902033df161531688ef7d1ca
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-06-10 19:08:12 +0800

    Add dependencies update script

--- src/Blazor.BrowserExtension/tool/BrotliDecodeJsProcessor.ps1
+++ src/Blazor.BrowserExtension/tool/BrotliDecodeJsProcessor.ps1
@@ -1,4 +1,7 @@
-﻿$jsPath = "../content/BrowserExtensionScripts/lib/decode.js"
+﻿param (
+    $jsPath
+)
+
 $jsContent = Get-Content $jsPath -Raw
 Function OctalToUnicode {
     param(
@@ -6,5 +9,6 @@ Function OctalToUnicode {
     )
     Return ([Convert]::ToString([Convert]::ToInt32($octal, 8), 16)).PadLeft(4, "0")
 }
-$jsContent = [regex]::Replace($jsContent, "\\(\d{1,3})" ,{param($match) "\u$(OctalToUnicode($match.Groups[1].Value))"})
-$jsContent | Set-Content $jsPath
+$jsContentHeader = "// This file has been transformed by tool/BrotliDecodeJsProcessor.ps1 to convert octal character literals to unicode character literals`r`n"
+$jsContent = $jsContentHeader + [regex]::Replace($jsContent, "\\(\d{1,3})" ,{param($match) "\u$(OctalToUnicode($match.Groups[1].Value))"})
+$jsContent | Set-Content $jsPath -NoNewline
---
+++ src/Blazor.BrowserExtension/tool/DependenciesUpdater.ps1
@@ -0,0 +1,44 @@
+﻿$relativeLibPath = "../content/BrowserExtensionScripts/lib"
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$libPath = Resolve-Path "$scriptPath/$relativeLibPath"
+
+If (-Not(Test-Path -Path $libPath)) {
+    Throw "Library path does not exist: $libPath"
+}
+
+Function DownloadFile {
+    param (
+        $baseUrl,
+        $fileName
+    )
+    Invoke-RestMethod -Uri "$baseUrl/$fileName" -OutFile "$libPath/$fileName"
+}
+
+Function UpdateBrowserPolyfill {
+    $baseUrl = "https://unpkg.com"
+    $request = [System.Net.WebRequest]::Create("$baseUrl/webextension-polyfill")
+    $request.AllowAutoRedirect = $false
+    $response = $request.GetResponse()
+    
+    If ($response.StatusCode -eq "Found") {
+        $versionRelativeUrl = $response.GetResponseHeader("Location")
+        $versionUrl = "$baseUrl$versionRelativeUrl/dist"
+        DownloadFile -baseUrl $versionUrl -fileName "browser-polyfill.js"
+        DownloadFile -baseUrl $versionUrl -fileName "browser-polyfill.js.map"
+        DownloadFile -baseUrl $versionUrl -fileName "browser-polyfill.min.js"
+        DownloadFile -baseUrl $versionUrl -fileName "browser-polyfill.min.js.map"
+    } Else {
+        Throw "Unable to get the latest version of webextension-polyfill."
+    }
+}
+
+Function UpdateBrotliDecode {
+    $baseUrl = "https://raw.githubusercontent.com/google/brotli/master/js"
+    DownloadFile -baseUrl $baseUrl -fileName "decode.js"
+    DownloadFile -baseUrl $baseUrl -fileName "decode.min.js"
+    & "$scriptPath/BrotliDecodeJsProcessor.ps1" -jsPath "$libPath/decode.js"
+    & "$scriptPath/BrotliDecodeJsProcessor.ps1" -jsPath "$libPath/decode.min.js"
+}
+
+UpdateBrowserPolyfill
+UpdateBrotliDecode
